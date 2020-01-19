c461b95a227ecf20627207ad90abdb744f0c0a74
pfefferniels <niels.pfeffer@gmail.com>
2020-01-19 15:18:34 +0100

    remove timeline again, add metronome

---
+++ public/js/inflections.js
@@ -0,0 +1,117 @@
+let vrvToolkit = new verovio.toolkit();
+points = [];
+
+$.ajax({
+  url: 'grieg_1903.svl',
+  dataType: 'text',
+  success: function (data) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, 'application/xml');
+    $(doc).find('point').each(function() {
+      points.push({
+        frame: parseInt($(this).attr('frame'), 10),
+        corresp: $(this).attr('label')
+      });
+    });
+
+    let draw = SVG().addTo('#svl_canvas');
+
+    let factor = 8;
+    let marginBottom = 40000;
+
+    // duration of semiquavers
+    let diff = [];
+    for (let i=0; i<points.length-1; i++) {
+      diff.push(points[i+1].frame - points[i].frame);
+    }
+
+    // duration of crotchets
+    let diff2 = [];
+    for (let i=0; i<points.length-4; i+=4) {
+      diff2.push(points[i+4].frame - points[i].frame);
+    }
+
+    let maxDiff = Math.max(...diff2) * factor;
+    let maxFrame = points[points.length-1].frame;
+    let minFrame = points[0].frame;
+    draw.viewbox(minFrame, 0, maxFrame, maxDiff+marginBottom);
+
+    // draw semiquavers
+    for (let i=0; i<points.length-1; i++) {
+      let corresp = points[i].corresp;
+      let verticalDiff = diff[i] * factor;
+
+      if (corresp == 'inaudible' || corresp == 'virtual') {
+        continue;
+      }
+
+      draw.rect(diff[i], verticalDiff).
+           move(points[i].frame, maxDiff - verticalDiff).
+           attr({
+            'fill': 'gray',
+            'stroke': '#000',
+            'stroke-width': 100
+           }).
+           id(corresp.slice(1) + '_instant').
+           on('mouseover', function() {
+             SVG(corresp).opacity(0.1).animate().opacity(1);
+           });
+    }
+
+    // draw crotchets
+    let barCount = 1;
+    for (let i=0; i<points.length-4; i+=4) {
+      let meterPos = (i/4)%4+1;
+      let verticalDiff = diff2[i/4] * factor;
+
+      let rect = draw.rect(diff2[i/4], verticalDiff).
+                      move(points[i].frame, maxDiff - verticalDiff).
+                      attr({
+                        'fill-opacity': 0.1,
+                        'stroke': '#000',
+                        'stroke-width': 300
+                      }).
+                      on('mouseover', function() {
+                        for (j=0; j<4; j++) {
+                          SVG(points[i+j].corresp).opacity(0.1).animate().opacity(1);
+                        }
+                      }).
+                      back();
+      draw.plain(meterPos).move(rect.cx(), rect.cy()).font({size: 9999});
+      if (meterPos == 4) {
+        let x = points[i].frame + rect.width();
+        draw.line(x, 0, x, maxDiff+marginBottom).attr({
+          'stroke-width': 1000,
+          'stroke': 'black',
+          'stroke-dasharray': 3000 });
+        //draw.plain(barCount).
+        //     move(points[i].frame - 0.5*points[i-15].frame, maxDiff + 0.5*marginBottom).
+        //     font({size: 9999});
+        barCount += 1;
+      }
+    }
+  }
+});
+
+$.ajax({
+  url: 'grieg_butterfly.mei',
+  dataType: "text",
+  success: function (data) {
+    let svg = vrvToolkit.renderData(data, {
+      scale: 20,
+      footer: 'none'
+      });
+    $("#mei_canvas").html(svg);
+
+    for (let i=0; i<points.length; i++) {
+      let frame = points[i].frame;
+      let corresp = $(points[i].corresp);
+      if (corresp.length != 0) {
+        corresp.on('mouseover', function() {
+          let instantSelector = '#' + $(this).attr('id') + '_instant';
+          let svg = SVG(instantSelector).opacity(0.1).animate().opacity(1);
+        });
+      }
+    }
+  }
+});
---
+++ public/js/metronome.js
@@ -0,0 +1,133 @@
+const frameRate = 44100;
+const noteLength = 0.05;    // length of a beep in seconds
+const scheduleAheadTime = 0.5; // How far ahead to schedule audio (sec)
+                            // This is calculated from lookahead, and overlaps
+                            // with next interval (in case the timer is late)
+const lookahead = 25.0;     // How frequently to call scheduling function (in ms)
+
+let audioContext = null;
+let unlocked = false;
+let isPlaying = false;      // Are we currently playing?
+let currentPoint;           // What note is currently last scheduled?
+let nextNoteTime = 0.0;     // when the next note is due.
+let lastPointDrawn = -1;    // the last "box" we drew on the screen
+let notesInQueue = [];      // the notes that have been put into the web audio,
+                            // and may or may not have played yet. {note, time}
+let timerWorker = null;     // The Web Worker used to fire timer messages
+
+
+// Add the duration of the current point and advance to the next time point
+function nextNote() {
+  let duration = (points[currentPoint+1].frame-points[currentPoint].frame) / frameRate;    // pick this from the points[]
+  nextNoteTime += duration;
+
+  currentPoint++;
+
+  // When all the points were played, jump back to the beginning.
+  if (currentPoint == points.length-1) {
+      currentPoint = 0;
+  }
+}
+
+function scheduleNote(beatNumber, time) {
+  // push the note on the queue, even if we're not playing.
+  notesInQueue.push( { note: beatNumber, time: time } );
+
+  // create an oscillator
+  var osc = audioContext.createOscillator();
+  osc.connect(audioContext.destination);
+  if (beatNumber % 16 === 0) {
+    osc.frequency.value = 880.0; // high pitch on 1st beat in measure
+  }
+  else if (beatNumber % 4 === 0 ) {
+    osc.frequency.value = 440.0; // medium pitch on the quarter notes
+  }
+  else {
+    osc.frequency.value = 220.0;  // low pitch on 16th notes
+  }
+
+  osc.start(time);
+  osc.stop(time + noteLength);
+}
+
+function scheduler() {
+  // while there are notes that will need to play before the next interval,
+  // schedule them and advance the pointer.
+  while (nextNoteTime < audioContext.currentTime + scheduleAheadTime) {
+    scheduleNote(currentPoint, nextNoteTime);
+    nextNote();
+  }
+}
+
+function play() {
+  if (!unlocked) {
+    // play silent buffer to unlock the audio
+    let buffer = audioContext.createBuffer(1, 1, 22050);
+    let node = audioContext.createBufferSource();
+    node.buffer = buffer;
+    node.start(0);
+    unlocked = true;
+  }
+
+  isPlaying = !isPlaying;
+
+  if (isPlaying) { // start playing
+      currentPoint = 0;
+      nextNoteTime = audioContext.currentTime;
+      timerWorker.postMessage("start");
+  } else {
+      timerWorker.postMessage("stop");
+  }
+
+  $('#play i').toggle();
+}
+
+function draw() {
+  let currentNote = lastPointDrawn;
+  const currentTime = audioContext.currentTime;
+
+  while (notesInQueue.length && notesInQueue[0].time < currentTime) {
+    currentNote = notesInQueue[0].note;
+    notesInQueue.splice(0,1); // remove note from queue
+  }
+
+  // We only need to draw if the note has moved.
+  if (lastPointDrawn != currentNote) {
+    let svg = SVG(points[currentNote].corresp + '_instant');
+    if (svg != null) {
+      svg.opacity(0).animate().opacity(1);
+    }
+    svg = SVG(points[currentNote].corresp);
+    if (svg != null) {
+      svg.opacity(0).animate().opacity(1);
+    }
+
+    lastPointDrawn = currentNote;
+  }
+
+  // set up to draw again
+  requestAnimationFrame(draw);
+}
+
+function init() {
+  audioContext = new AudioContext();
+
+  requestAnimationFrame(draw);
+
+  timerWorker = new Worker("js/metronomeworker.js");
+  timerWorker.onmessage = function(e) {
+    if (e.data == "tick") {
+      scheduler();
+    }
+    else {
+      console.log("message: " + e.data);
+    }
+  };
+  timerWorker.postMessage({"interval": lookahead});
+}
+
+$(document).ready(function() {
+  init();
+
+  $('#play').on('click', play);
+});
